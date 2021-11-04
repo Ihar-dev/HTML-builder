7b75096dab15313feb0e4011707e01aac91ceb11
Ihar Kozik <ihar.kozik@gmail.com>
2021-11-04 22:15:12 +0300

    feat: add task3 solution

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const fs = require('fs');
+const directory = __dirname + '/secret-folder';
+/* let dirBuf = Buffer.from(directory); */
+fs.readdir(directory, (err, files) => {
+  if (err) throw err;
+  process.stdout.write('Files list:\n');
+  for (let file of files) {
+    let filePath = path.join(directory, file);
+    fs.stat(filePath, (err, stats) => {
+      if (err) throw err;
+      if (!stats.isDirectory()) {
+        process.stdout.write(`${path.parse(filePath).name}`);
+        process.stdout.write(` - ${path.parse(filePath).ext.substr(1)}`);
+        process.stdout.write(` - ${(stats.size * 0.001).toFixed(3)}kb\n`);
+      }
+    })
+  }
+})
