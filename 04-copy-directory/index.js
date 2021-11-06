7a1f331a8cd0789f50b7291e2d77e4e61b171d84
Ihar Kozik <ihar.kozik@gmail.com>
2021-11-06 20:38:19 +0300

    feat: edit task4 solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,35 +3,54 @@ const path = require('path');
 const prevDir = path.join(__dirname, 'files');
 const nextDir = path.join(__dirname, 'files-copy');
 const fsPromises = fs.promises;
-fsPromises.mkdir(nextDir, {
-  recursive: true
-});
-fs.readdir(nextDir, (err, files) => {
-  if (err) throw err;
-  for (let file of files) {
-    let nextFilePath = path.join(nextDir, file);
-    fs.stat(nextFilePath, (err, stats) => {
+const makeDirectory = async () => {
+  try {
+    fsPromises.mkdir(nextDir, {
+      recursive: true
+    });
+    clearDirectory();
+  } catch (err) {
+    if (err) throw err;
+  }
+}
+const clearDirectory = async () => {
+  try {
+    fs.readdir(nextDir, (err, files) => {
       if (err) throw err;
-      if (!stats.isDirectory()) {
-        fs.unlink(nextFilePath, err => {
+      for (let file of files) {
+        let nextFilePath = path.join(nextDir, file);
+        fs.stat(nextFilePath, (err, stats) => {
           if (err) throw err;
+          if (!stats.isDirectory()) {
+            fs.unlink(nextFilePath, err => {
+              if (err) throw err;
+            });
+          }
         });
       }
-    });
-  }
-})
-setTimeout(() => {
-  fs.readdir(prevDir, (err, files) => {
+    })
+    copyDirectory();
+  } catch (err) {
     if (err) throw err;
-    for (let file of files) {
-      let prevFilePath = path.join(prevDir, file);
-      let nextFilePath = path.join(nextDir, file);
-      fs.stat(prevFilePath, (err, stats) => {
-        if (err) throw err;
-        if (!stats.isDirectory()) {
-          fsPromises.copyFile(prevFilePath, nextFilePath);
-        }
-      })
-    }
-  })
-}, 1000);
+  }
+}
+const copyDirectory = async () => {
+  try {
+    fs.readdir(prevDir, (err, files) => {
+      if (err) throw err;
+      for (let file of files) {
+        let prevFilePath = path.join(prevDir, file);
+        let nextFilePath = path.join(nextDir, file);
+        fs.stat(prevFilePath, (err, stats) => {
+          if (err) throw err;
+          if (!stats.isDirectory()) {
+            fsPromises.copyFile(prevFilePath, nextFilePath);
+          }
+        })
+      }
+    })
+  } catch (err) {
+    if (err) throw err;
+  }
+}
+makeDirectory();
