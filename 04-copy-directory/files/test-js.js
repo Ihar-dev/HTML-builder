dffc5407745bd5cce14985fb2a3962cdc4dc00b9
Ihar Kozik <ihar.kozik@gmail.com>
2021-11-06 19:39:04 +0300

    feat: add task4 solution

--- 04-copy-directory/files/test-js.js
+++ 04-copy-directory/files/test-js.js
@@ -1,1 +1,3 @@
-console.log('Hello test');
+function copyDir (currentDir) {
+
+}
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,37 @@
-console.log('click');
+const fs = require('fs');
+const path = require('path');
+const prevDir = path.join(__dirname, 'files');
+const nextDir = path.join(__dirname, 'files-copy');
+const fsPromises = fs.promises;
+fsPromises.mkdir(nextDir, {
+  recursive: true
+});
+fs.readdir(nextDir, (err, files) => {
+  if (err) throw err;
+  for (let file of files) {
+    let nextFilePath = path.join(nextDir, file);
+    fs.stat(nextFilePath, (err, stats) => {
+      if (err) throw err;
+      if (!stats.isDirectory()) {
+        fs.unlink(nextFilePath, err => {
+          if (err) throw err;
+        });
+      }
+    });
+  }
+})
+setTimeout(() => {
+  fs.readdir(prevDir, (err, files) => {
+    if (err) throw err;
+    for (let file of files) {
+      let prevFilePath = path.join(prevDir, file);
+      let nextFilePath = path.join(nextDir, file);
+      fs.stat(prevFilePath, (err, stats) => {
+        if (err) throw err;
+        if (!stats.isDirectory()) {
+          fsPromises.copyFile(prevFilePath, nextFilePath);
+        }
+      })
+    }
+  })
+}, 1000);
