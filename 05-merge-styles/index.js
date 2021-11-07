7778d4dd37dfc4cc8f33a6d85102fa9889a37d54
Ihar Kozik <ihar.kozik@gmail.com>
2021-11-07 16:22:56 +0300

    fix: edit task5 solution

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -3,49 +3,37 @@ const path = require('path');
 const prevDir = path.join(__dirname, 'styles');
 const filePath = path.join(__dirname, 'project-dist', 'bundle.css');
 const arr = [];
-const copyStyles = async () => {
-  try {
-    fs.readdir(prevDir, (err, files) => {
-      if (err) throw err;
-      for (let file of files) {
-        let prevFilePath = path.join(prevDir, file);
-        fs.stat(prevFilePath, (err, stats) => {
-          if (err) throw err;
-          if (!stats.isDirectory() && path.parse(prevFilePath).ext === '.css') {
-            const myReadStream = fs.createReadStream(prevFilePath);
-            myReadStream.on('data', chunk => {
-              const textData = '' + Buffer.from(chunk);
-              arr.push(textData);
-            });
-          }
-        })
-      }
-      setTimeout(makeBundle);
-    })
-  } catch (err) {
+const copyStyles = () => {
+  fs.readdir(prevDir, (err, files) => {
     if (err) throw err;
-  }
+    for (let file of files) {
+      let prevFilePath = path.join(prevDir, file);
+      fs.stat(prevFilePath, (err, stats) => {
+        if (err) throw err;
+        if (!stats.isDirectory() && path.parse(prevFilePath).ext === '.css') {
+          const myReadStream = fs.createReadStream(prevFilePath);
+          myReadStream.on('data', chunk => {
+            const textData = '' + Buffer.from(chunk);
+            arr.push(textData);
+          });
+        }
+      })
+    }
+    setTimeout(makeBundle);
+  })
 }
-const makeBundle = async () => {
-  try {
-    fs.writeFile(filePath, '', err => {
-      if (err) throw err;
-      copyToBundle();
-    });
-  } catch (err) {
+const makeBundle = () => {
+  fs.writeFile(filePath, '', err => {
     if (err) throw err;
-  }
+    if (arr.length > 0) copyToBundle();
+  });
 }
 let i = 0;
-const copyToBundle = async () => {
-  try {
-    fs.appendFile(filePath, arr[i], err => {
-      if (err) throw err;
-      i++;
-      if (i < arr.length) copyToBundle();
-    });
-  } catch (err) {
+const copyToBundle = () => {
+  fs.appendFile(filePath, arr[i], err => {
     if (err) throw err;
-  }
+    i++;
+    if (i < arr.length) copyToBundle();
+  });
 }
 copyStyles();
