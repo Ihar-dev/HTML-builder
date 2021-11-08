71831396d83afc905fc44dc46a50298c8c7c19fd
Ihar Kozik <ihar.kozik@gmail.com>
2021-11-08 17:11:14 +0300

    fix: edit task2 solution

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,10 +1,6 @@
 const path = require('path');
 const fs = require('fs');
 const filePath = __dirname + '/text.txt';
-fs.writeFile(filePath, '', err => {
-  if (err) throw err;
-  process.stdout.write('File "text.txt" has been added!\n');
-});
 const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
@@ -21,15 +17,17 @@ const enterYouText = (promptText, isNew) => {
     } else {
       fs.appendFile(filePath, textStarting + userInput, err => {
         if (err) throw err;
-        enterYouText(''/* userInput + ' added!\nYou can enter your text again or type "exit" (press "ctrl + c") to quit:\n' */);
+        enterYouText('' /* userInput + ' added!\nYou can enter your text again or type "exit" (press "ctrl + c") to quit:\n' */ );
       });
     }
   });
 }
+fs.writeFile(filePath, '', err => {
+  if (err) throw err;
+  process.stdout.write('File "text.txt" has been added!\n');
+  enterYouText('Enter your text or type "exit" (press "ctrl + c") to quit:\n', 'new');
+});
 rl.on('close', () => {
   if (closeText) process.stdout.write('You typed "exit". Goodbye!\n');
   else process.stdout.write('You pressed "ctrl + c". Goodbye!\n');
 });
-setTimeout(() => {
-  enterYouText('Enter your text or type "exit" (press "ctrl + c") to quit:\n', 'new');
-}, 500);
